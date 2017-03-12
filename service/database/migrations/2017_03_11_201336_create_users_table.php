f95375ecfd0afcaa39a4e7792ffed7a8970c2861
jasperboyd <jasperboyd@icloud.com>
2017-03-12 16:48:31 -0400

    adding a token field to the users table

--- service/database/migrations/2017_03_11_201336_create_users_table.php
+++ service/database/migrations/2017_03_11_201336_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->timestamps();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('token');
             $table->string('password');
         });
     }
